fd6e848bc557824b479d2169780cbf70026c35b4
arielpriyael <117847676+arielpriyael@users.noreply.github.com>
2022-11-12 14:54:32 +0700

    Add files via upload

---
+++ BMI/src/main/java/com/mycompany/bmi/BMI.java
@@ -0,0 +1,35 @@
+
+package com.mycompany.bmi;
+
+import java.util.Scanner;
+public class BMI {
+
+    public static void main(String[] args) {
+      Scanner alim = new Scanner (System.in);
+      int berat;
+      double tinggi;
+      double bmi;
+      
+      System.out.println("masukkan berat badan : ");
+      berat = alim.nextInt();
+      System.out.println("masukkan tinggi badan : ");
+      tinggi = alim.nextDouble();
+      tinggi/=100;
+      bmi = berat/(tinggi*tinggi);
+      System.out.println("bmi :"+bmi);
+      
+      if(bmi > 27){
+           System.out.println("obesitas");
+      } 
+      else if(bmi > 24.9){
+           System.out.println("kelebihan berat badan");
+      }
+      else if(bmi > 18.9){
+           System.out.println("normal");
+      }
+      else{
+           System.out.println("kurus");
+      }
+    }
+    
+}
